d1d03e34129c4e123799383c5fb02cb5966ee7f3
mano <peerapat.a@extend-it-resource.com>
2024-12-08 14:51:31 +0700

    config image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["d38b044pevnwc9.cloudfront.net"], // Replace with actual logo source domains if not hosted locally.
+  },
 };
 
 export default nextConfig;
